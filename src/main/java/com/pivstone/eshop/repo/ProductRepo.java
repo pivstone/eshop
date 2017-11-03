6740cf967faf79aa5a2e4680306a85a1ae1513e4
xbeco <pivstone@gmail.com>
2017-11-03 11:54:39 +0800

    Transform ProductRepo from JpaRepository To PagingAndSortingRepository

--- src/main/java/com/pivstone/eshop/repo/ProductRepo.java
+++ src/main/java/com/pivstone/eshop/repo/ProductRepo.java
@@ -1,7 +1,9 @@
 package com.pivstone.eshop.repo;
 
 import com.pivstone.eshop.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.Collection;
 import java.util.UUID;
@@ -10,6 +12,8 @@ import java.util.UUID;
  * Mail: pivstone@gmail.com <br>
  * Created by pivstone on 2017/11/3.
  */
-public interface ProductRepo extends JpaRepository<Product, UUID> {
+public interface ProductRepo extends PagingAndSortingRepository<Product, UUID> {
     Collection<Product> findByCategoryId(UUID categoryID);
+
+    Page<Product> findByCategoryId(UUID categoryID, Pageable pageable);
 }
