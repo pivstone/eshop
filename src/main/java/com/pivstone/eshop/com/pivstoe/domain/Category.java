dddd19616056c9306f7c7faf87f1ea34c2715fc7
xbeco <pivstone@gmail.com>
2017-11-03 10:01:00 +0800

    create entity class

---
+++ src/main/java/com/pivstone/eshop/com/pivstoe/domain/Category.java
@@ -0,0 +1,14 @@
+package com.pivstone.eshop.com.pivstoe.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+/**
+ * Mail: pivstone@gmail.com <br>
+ * Created by pivstone on 2017/11/3.
+ */
+@Data
+@Entity
+public class Category {
+}
---
+++ src/main/java/com/pivstone/eshop/com/pivstoe/domain/Product.java
@@ -0,0 +1,15 @@
+package com.pivstone.eshop.com.pivstoe.domain;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+/**
+ * Mail: pivstone@gmail.com <br>
+ * Created by pivstone on 2017/11/3.
+ */
+@Data
+@Entity
+public class Product {
+
+}
