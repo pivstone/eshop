0a08fe22c9dfe83f4c453edf20474ea022056577
xbeco <pivstone@gmail.com>
2017-11-06 01:09:23 +0800

    Abstract Resource

--- src/main/java/com/pivstone/eshop/resource/CategoryResource.java
+++ src/main/java/com/pivstone/eshop/resource/CategoryResource.java
@@ -2,7 +2,6 @@ package com.pivstone.eshop.resource;
 
 import com.pivstone.eshop.controller.CategoryController;
 import com.pivstone.eshop.model.Category;
-import org.springframework.hateoas.Resource;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -11,16 +10,11 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
  * Mail: pivstone@gmail.com <br>
  * Created by pivstone on 2017/11/4.
  */
-public class CategoryResource extends Resource<Category> {
-    private final Category category;
+public class CategoryResource extends AbstractResource<Category> {
 
 
     public CategoryResource(Category category) {
         super(category);
-        this.category = category;
-        this.add(linkTo(methodOn(CategoryController.class).show(category.getId())).withSelfRel());
-        this.add(linkTo(methodOn(CategoryController.class).index(null, null)).withRel("list"));
         this.add(linkTo(methodOn(CategoryController.class).products(category.getId(), null, null)).withRel("products"));
-
     }
 }
