3bc279e94637a4e11fe8ce54118474a5f3345666
xbeco <pivstone@gmail.com>
2017-11-06 01:13:56 +0800

    Remove abstract resource

--- src/main/java/com/pivstone/eshop/resource/CategoryResource.java
+++ src/main/java/com/pivstone/eshop/resource/CategoryResource.java
@@ -2,6 +2,7 @@ package com.pivstone.eshop.resource;
 
 import com.pivstone.eshop.controller.CategoryController;
 import com.pivstone.eshop.model.Category;
+import org.springframework.hateoas.Resource;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -10,11 +11,12 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
  * Mail: pivstone@gmail.com <br>
  * Created by pivstone on 2017/11/4.
  */
-public class CategoryResource extends AbstractResource<Category> {
-
+public class CategoryResource extends Resource<Category> {
 
     public CategoryResource(Category category) {
         super(category);
+        this.add(linkTo(methodOn(CategoryController.class).show(category.getId())).withSelfRel());
+        this.add(linkTo(methodOn(CategoryController.class).index(null, null)).withRel("list"));
         this.add(linkTo(methodOn(CategoryController.class).products(category.getId(), null, null)).withRel("products"));
     }
 }
--- src/main/java/com/pivstone/eshop/resource/ProductResource.java
+++ src/main/java/com/pivstone/eshop/resource/ProductResource.java
@@ -1,14 +1,21 @@
 package com.pivstone.eshop.resource;
 
+import com.pivstone.eshop.controller.ProductController;
 import com.pivstone.eshop.model.Product;
+import org.springframework.hateoas.Resource;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  * Mail: pivstone@gmail.com <br>
  * Created by pivstone on 2017/11/4.
  */
-public class ProductResource extends AbstractResource<Product> {
+public class ProductResource extends Resource<Product> {
 
     public ProductResource(Product entity) {
         super(entity);
+        this.add(linkTo(methodOn(ProductController.class).show(entity.getId())).withSelfRel());
+        this.add(linkTo(methodOn(ProductController.class).index(null, null)).withRel("list"));
     }
 }
