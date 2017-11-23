8d41c882f8b351112978186f85d88e1a5d773b7c
xbeco <pivstone@gmail.com>
2017-11-23 12:10:13 +0800

    Repo should be singleton & stateless. 1、Repo instance is heavy object 2、Repo is thread-safe object 3、Repo should be stateless 4、Repo has connection pool, if a exception break, the connection will be recycle 5、if some connection was idle too long time, closed by driver, the connection pool will deal with it

--- src/main/java/com/pivstone/eshop/jpa/RestRepo.java
+++ src/main/java/com/pivstone/eshop/jpa/RestRepo.java
@@ -1,9 +1,7 @@
 package com.pivstone.eshop.jpa;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -13,7 +11,6 @@ import java.util.UUID;
  * Created by pivstone on 2017/11/6.
  */
 @NoRepositoryBean
-@Scope(WebApplicationContext.SCOPE_REQUEST)
 public interface RestRepo<E>  extends PagingAndSortingRepository<E, UUID> {
     Optional<E> findByName(String name);
     Optional<E> findById(UUID id);
